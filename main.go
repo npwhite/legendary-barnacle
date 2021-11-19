82f2d80dba30e30b18e12a9543d896647fbdfb6e
Nicholas Whitefield <whitefie@ualberta.ca>
2021-11-18 23:05:00 -0700

    temp removed jokes for now

--- main.go
+++ main.go
@@ -1,31 +1,52 @@
+// package main
+//
+// import (
+// 	"flag"
+// 	"fmt"
+//
+// 	"github.com/npwhite/legendary-barnacle/jokes"
+// )
+//
+// // legal joke types
+// const (
+// 	GeneralJoke     = "general"
+// 	ProgrammingJoke = "programming"
+// )
+//
+// var flag_jokeType string
+//
+// func init() {
+// 	flag.StringVar(&flag_jokeType, "type", GeneralJoke, "The type of joke to get. Must be one of: general, programming")
+// 	flag.Parse()
+// }
+//
+// func main() {
+// 	joke, err := jokes.RandomJokeOfType(flag_jokeType)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 		return
+// 	}
+// 	fmt.Println(joke.Setup)
+// 	fmt.Println(joke.Punchline)
+// }
+
 package main
 
 import (
-	"flag"
 	"fmt"
-
-	"github.com/npwhite/legendary-barnacle/jokes"
+	"time"
+	"math/rand"
 )
 
-// legal joke types
-const (
-	GeneralJoke     = "general"
-	ProgrammingJoke = "programming"
-)
-
-var flag_jokeType string
-
-func init() {
-	flag.StringVar(&flag_jokeType, "type", GeneralJoke, "The type of joke to get. Must be one of: general, programming")
-	flag.Parse()
+var greetings = []string{
+	"hello!",
+	"hey!",
+	"welcome!",
+	"hello there!",
 }
 
 func main() {
-	joke, err := jokes.RandomJokeOfType(flag_jokeType)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	fmt.Println(joke.Setup)
-	fmt.Println(joke.Punchline)
+	rand.Seed(time.Now().UnixNano())
+	g := greetings[rand.Intn(len(greetings))]
+	fmt.Println(g)
 }
